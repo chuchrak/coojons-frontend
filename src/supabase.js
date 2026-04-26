fe078f1714bb304cd39306cdeb863f4434a20c2c
Nikodem <nprzybysawski@gmail.com>
2026-04-26 21:59:48 +0200

    asda#

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Te dane znajdziesz w Supabase -> Project Settings -> API
-const supabaseUrl = 'https://gcaugzcvwfwtmukmgiq.supabase.co'
+const supabaseUrl = 'gcaugzcvwfwytmukmgiq'
 const supabaseAnonKey = 'sb_publishable_VI4cwSrJSd8P4XjTqT9xSQ_YtQk27TT'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
