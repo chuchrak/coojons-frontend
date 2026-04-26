91a2beb2d996a6984c353a685cfa959d735bbff7
Nikodem <nprzybysawski@gmail.com>
2026-04-26 22:24:32 +0200

    adsad

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Upewnij się, że URL zaczyna się od https://
-const supabaseUrl = 'https://gcaugzcvwfytmukmgiq.supabase.co'
+const supabaseUrl = 'https://gcaugzcvwfwytmukmgiq.supabase.co/rest/v1/'
 
 // Tutaj wklej swój klucz ANON (ten, który zaczyna się od sb_publishable...)
 const supabaseAnonKey = 'sb_publishable_VI4cwSrJSd8P4XjTqT9xSQ_YtQk27TT'
